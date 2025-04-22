5606b764e94bd88db48f14c34ca955fbc0f48eea
Jerron Lim <l.y.z.jerron@gmail.com>
2025-04-22 21:58:20 +0800

    feat: Integrate Vercel Analytics for enhanced tracking and insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: "ReceiptWiser",
@@ -13,7 +14,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="antialiased font-sans">{children}</body>
+      <body className="antialiased font-sans">
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
